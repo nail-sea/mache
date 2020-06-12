1fbfe08eb9e09d8b572a282d019c75e0c8d8c0ef
xuliangkai <18618287120@139.com>
2020-06-12 16:37:40 +0800

    test branch

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-//我是dev
+//我是dev 我是第二次的dev
 function Welcome(props) {
 return <h1>Hello, {props.name}</h1>
 }
 
-const element = <Welcome name="Sara" />
+const element = <Welcome name="Saraddddddd" />
 ReactDOM.render(
   element,
   document.getElementById('root')
